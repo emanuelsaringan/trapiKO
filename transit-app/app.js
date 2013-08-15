75fdc2b028c87d6a81b46b9560c08765ea36d4a8
Emanuel Saringan <dev@lambdageek.com>
2013-08-15 22:56:30 +0800

    Tried a minimalistic theme. Added 404 and 500 error pages.

--- transit-app/app.js
+++ transit-app/app.js
@@ -45,6 +45,71 @@ app.get('/about',
   }
 );
 
+app.get('/blog', 
+  function(req, res) {
+    res.render('blog', {
+      title: 'trapiKO Blog'
+    });
+  }
+);
+
+app.get('/contact', 
+  function(req, res) {
+    res.render('contact', {
+      title: 'Contact Us'
+    });
+  }
+);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Error handling
+
+app.use(app.router);
+
+app.use(
+  function(req, res, next) {
+    res.status(404);
+
+    if (req.accepts('html')) {
+      res.render('404', {url : req.url});
+      return;
+    }
+
+    if (req.accepts('json')) {
+      res.send({error : 'Not founds'});
+      return;
+    }
+
+    res.type('txt').send('Not foundz');
+  }
+);
+
+app.use(
+  function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('500', {error : err});
+  }
+);
+
+app.get('/404', 
+  function(req, res, next) {
+    next();
+  }
+);
+
+app.get('/403',
+  function(req, res, next) {
+    var err = new Error('not allowed!');
+    err.status = 403;
+    next(err);
+  }
+);
+
+app.get('/500',
+  function(req, res, next) {
+    next(new Error('keyboard cat!'));
+  }
+);
--- transit-app/public/javascripts/navbar.js
+++ transit-app/public/javascripts/navbar.js
@@ -2,7 +2,6 @@ $(document).ready(
   function() {
     var url = window.location.href;
     var curView = url.substring(url.lastIndexOf("/") + 1);
-    console.log(curView);
 
     if (curView == "") {
       $("#home_navlink").addClass("active");
